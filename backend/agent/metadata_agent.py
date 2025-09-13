10ded96bc9a520de07ae047326d0ddb0b103e60f
Arjithar8 <arjithar8@gmail.com>
2025-09-14 03:28:55 +0530

    Adding a metadata agent as having extra context on product and company info might make diffs a bit more accurate as we might be able to find the T&C page even if url changed

---
+++ backend/agent/metadata_agent.py
@@ -0,0 +1,18 @@
+from google.adk.agents import Agent
+from pydantic import BaseModel
+
+class MetaData(BaseModel):
+    product_name: str | None = None
+    company_name: str | None = None
+
+metadata_agent = Agent(
+    name="metadata-agent",
+    model="gemini-2.5-flash",
+    description="Agent to extract metadata from HTML content.",
+    instruction="""
+    You are a helpful agent that can extract metadata from HTML content and return it in JSON format.
+    Given the head section of an HTML document, you will extract information such as product_name and company_name if available.
+    If the information is not available, return null for that field.
+    """,
+    output_schema=MetaData
+)
