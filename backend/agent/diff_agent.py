e13110fe4e2d10dc4aa2e244066751072e1407e5
Arjithar8 <arjithar8@gmail.com>
2025-09-20 17:25:54 +0530

    Bug fix for diff agent and update on system prompt for root agent

--- backend/agent/diff_agent.py
+++ backend/agent/diff_agent.py
@@ -3,7 +3,7 @@ from google.adk.agents import Agent
 
 def diff_agent() -> Agent:
     return Agent(
-        name="diff-agent",
+        name="diff_agent",
         model="gemini-2.5-flash",
         description="Agent that highlights differences between past and current versions of a terms page.",
         instruction="""
--- backend/agent/root_agent.py
+++ backend/agent/root_agent.py
@@ -7,7 +7,8 @@ def root_agent() -> Agent:
         description="Agent to answer questions about terms and conditions of services.",
         instruction="""
         You are a helpful agent that can answer questions about terms and conditions of services.
-        If user provides a URL, parse it.
-        If no URL is provided, use Google Search to find the terms and conditions page url, then parse it.
+        If a user provides url a parsed html context will be provided to you.
+        If its not the first time url was provided, you will also get a summary of changes that happened since last time this information.
+        This changes information should be provided in the end of your answer with > ⚠️ Notice: <summary of changes>.
         """,
     )
